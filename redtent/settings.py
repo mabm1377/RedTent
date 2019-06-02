c4dd134fcd835f6c52c5a592bb772410af301dba
alibashari <mabm13770@gmail.com>
2019-06-02 20:25:47 +0430

    delete doc

--- redtent/settings.py
+++ redtent/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'user_account',
     'images',
     'sales_history',
-    'django_extensions',
     'corsheaders'
 ]
 
--- redtent/urls.py
+++ redtent/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     path(r'rate_for_designer/', include('rate_for_designer.urls')),
     path(r'comment_for_designer/', include('comment_for_designer.urls')),
     path(r'rate_for_tags/', include('user_account.urls')),
-    path(r'files/', include('images.urls')),
-    path(r'docs/', include_docs_urls(title="my doc"))
+    path(r'files/', include('images.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
